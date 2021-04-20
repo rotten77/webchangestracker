aff78a2344b2186adc8b1a95c51e67c6955bb5d2
Jan Zatloukal <zatloukal.jan@gmail.com>
2021-04-20 06:36:38 +0200

    Add more details to index list

--- index.php
+++ index.php
@@ -7,27 +7,11 @@ include dirname(__FILE__) . "/app/app.login.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WebChangesTracker</title>
-    <link rel="stylesheet" href="./style.css?v=4">
+    <link rel="stylesheet" href="./style.css?v=5">
 </head>
 <body>
 <h1>WebChangesTracker</h1>
-<table>
-<tbody>
 
-<?php
-foreach($db->website() as $website) {
-    echo '<tr>';
-    echo '      <td><b>'.$website['label'].'</b></td>';
-    echo '      <td><a href="'.$website['url'].'">Open</a></td>';
-    echo '      <td><a href="./view.php?id='.$website['id'].'">Preview</a></td>';
-    echo '      <td><a href="./editor/index.php?username='.urlencode(EMAIL_ADDRESS).'&amp;edit=website&amp;'.urlencode('where[id]').'='.$website['id'].'">Edit</a></td>';
-    echo '</tr>';
-}
-?>
-</tbody>
-</table>
-
-<hr />
 <a href="./editor/index.php?username=<?php echo urlencode(EMAIL_ADDRESS); ?>">Editor</a> &#124;
 <a href="./editor/index.php?username=<?php echo urlencode(EMAIL_ADDRESS); ?>&amp;edit=website">Add new website</a> &#124;
 <a href="./?logout">Logout</a> &#124;
@@ -38,5 +22,42 @@ foreach($db->website() as $website) {
 <?php } else { ?>
     <a href="./?run">Run tracker manualy</a>
 <?php } ?>
+
+<hr />
+
+
+<table>
+
+<thead>
+<tr>
+    <th>Label</th>
+    <th>Status</th>
+    <th>Interval</th>
+    <th>Last tracking</th>
+    <th>URL</th>
+    <th>Preview</th>
+    <th>Edit</th>
+</tr>
+</thead>
+<tbody>
+
+<?php
+foreach($db->website() as $website) {
+    echo '<tr>';
+    echo '      <td><b>'.$website['label'].'</b></td>';
+    echo '      <td>'.$website['status'].'</td>';
+    echo '      <td>'.$website['tracking_interval'].'</td>';
+    echo '      <td>'.$website['tracking_last'].'</td>';
+    echo '      <td><a href="'.$website['url'].'">URL</a></td>';
+    echo '      <td><a href="./view.php?id='.$website['id'].'">Preview</a></td>';
+    echo '      <td><a href="./editor/index.php?username='.urlencode(EMAIL_ADDRESS).'&amp;edit=website&amp;'.urlencode('where[id]').'='.$website['id'].'">Edit</a></td>';
+    echo '</tr>';
+
+}
+?>
+</tbody>
+</table>
+
+
 </body>
 </html>
